32ac80e25b59b22b8873729a6a28fa88833895a8
hockey4life63 <Amelancon68@gmail.com>
2017-05-31 12:32:03 -0700

    testing

---
+++ main.js
@@ -0,0 +1,122 @@
+let blackCardsRef = firebase.database().ref("cards/blackCards");
+let whiteCardsRef = firebase.database().ref("cards/whiteCards");
+let database = firebase.database();
+let cardRef = database.ref("/cards");
+let blackCardRef = cardRef.child("/blackCards");
+let whiteCardRef = cardRef.child("/whiteCards");
+
+
+function callDictApi(message) {
+    let baseURL = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/"
+    let endURL = "/definitions"
+
+    $.ajax({
+        url: "https://owlbot.info/api/v1/dictionary/owl?format=json",
+
+        // headers: {
+        //     "app_id": "bc40b411",
+        //     "app_key": "1060b1b34f95cf524f8c88b7c9edb7ba"
+        // }
+        //OR
+        //beforeSend: function(xhr) { 
+        //  xhr.setRequestHeader("My-First-Header", "first value"); 
+        //  xhr.setRequestHeader("My-Second-Header", "second value"); 
+        //}
+    }).done(function(data) {
+        console.log(data)
+    });
+}
+// function shuffleCards(key) {
+//     //TODO: add teh arrays to the currentGame database;
+//     let whiteCount = 0;
+//     let blackCount = 0;
+//     let tempArray = {
+//         white: [],
+//         black: []
+//     };
+//     let shuffledArray = {
+//         white: [],
+//         black: []
+//     };
+//     cardRef.child("whiteCardCount").once("value", function(snap) {
+//         whiteCount = snap.val();
+//     }).then(function() {
+//         cardRef.child("blackCardCount").once("value", function(snap) {
+//             blackCount = snap.val();
+//         }).then(function() {
+//             for (var i = 0; i < whiteCount; i++) {
+//                 tempArray.white.push(i);
+//             }
+//             for (var i = 0; i < blackCount; i++) {
+//                 tempArray.black.push(i);
+//             }
+//             for (var i = 0; i < blackCount; i++) {
+//                 let rand = Math.floor(Math.random() * tempArray.black.length);
+//                 shuffledArray.black.push(tempArray.black[rand]);
+//                 tempArray.black.slice(rand, 1);
+//             }
+//             for (var i = 0; i < whiteCount; i++) {
+//                 let rand = Math.floor(Math.random() * tempArray.white.length);
+//                 shuffledArray.white.push(tempArray.white[rand]);
+//                 tempArray.white.slice(rand, 1);
+//             }
+//             console.log(shuffledArray.white)
+//         })
+//     })
+
+
+
+
+// function setWhiteCards() {
+//     let set = 0;
+//     let count = 0;
+//     let total = 0;
+//     while (count < baseSet.whiteCards.length) {
+//         for (var i = 0; i < 50; i++) {
+//             if (baseSet.whiteCards[count]) {
+//                 whiteCardsRef.child(set).child(i).set(baseSet.whiteCards[count]);
+//                 total++
+//             } //if
+//             count++;
+//         } //for
+//         set++;
+//     } //while
+//     cardRef.update({
+//         whiteCardCount: total
+//     })
+// }
+
+// function setBlackCards() {
+//     let set = 0;
+//     let count = 0;
+//     let total = 0;
+//     while (count < baseSet.blackCards.length) {
+//         for (var i = 0; i < 50; i++) {
+//             if (baseSet.blackCards[count]) {
+//                 blackCardsRef.child(set).child(i).set(baseSet.blackCards[count]);
+//                 total++
+//             } //if
+//             count++;
+//         } //for
+//         set++;
+//     } //while
+//     cardRef.update({
+//         blackCardCount: total
+//     })
+// }
+// setWhiteCards();
+// setBlackCards();
+
+
+
+//how to convert to location
+/*
+ex.
+    card= 123
+    a= Math.floor(card/50);
+    b= card%50;
+    [a][b]
+    whiteCardRef.child(a).child(b).once("value", function(snap){
+    //so something
+    })
+*/
